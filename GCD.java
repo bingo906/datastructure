aaf4f5cd646e52860fcf44d7ba3ca3679371a1ef
Bingo <906014227@qq.com>
2017-04-17 13:50:28 +0800

    欧几里得求最大公约数

--- GCD.java
+++ GCD.java
@@ -7,14 +7,9 @@ public class GCD {
 		int gcd =1;
 		if(m%n==0){
 			return n;
-		} 
-		for(int k=n/2;k>=1;k--){
-			if(m%k==0 && n%k==0){
-				gcd = k;
-				break;
-			}
+		} else {
+			return gcd(n,m%n);
 		}
-		return gcd;
 	}
 	
 	public static void main(String[] args) {
